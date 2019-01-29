d46e978ab172fb8c32e14e7d8ec083c0daca477a
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-01-29 19:12:31 +0300

    better balance was updated

--- src/main/scala/ru/tolsi/blockchain/pos/PosTestApp.scala
+++ src/main/scala/ru/tolsi/blockchain/pos/PosTestApp.scala
@@ -11,7 +11,7 @@ object PosTestApp extends App with StrictLogging {
   val genesis = PosBlock.Genesis
   val (alicePk, alicePub) = Curve25519.createKeyPair(Array(1,3,5))
   val (bobPk, bobPub) = Curve25519.createKeyPair(Array(5,2,1))
-  val startBlockchain = PosBlockchain(Seq(genesis), Map(ByteStr(alicePub) -> 1000000, ByteStr(bobPub) -> 2000000), 10.seconds)
+  val startBlockchain = PosBlockchain(Seq(genesis), Map(ByteStr(alicePub) -> 10000000, ByteStr(bobPub) -> 20000000), 10.seconds)
   logger.info(s"Start from $startBlockchain")
   val miner = new PosMiner(ByteStr(alicePk), ByteStr(alicePub))
   miner.minerTask(startBlockchain).runAsyncAndForget
