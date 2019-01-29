458f8cd5adac7a3650eb3289b96cd9a097429947
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-01-29 15:35:52 +0300

    params typo fix

--- src/main/scala/ru/tolsi/blockchain/Blockchain.scala
+++ src/main/scala/ru/tolsi/blockchain/Blockchain.scala
@@ -1,9 +1,7 @@
 package ru.tolsi.blockchain
 
-import ru.tolsi.blockchain.pow.PowBlock
-
 trait Blockchain[B <: Block, BC <: Blockchain[B, BC]] {
   def blocks: Seq[B]
-  def append(block: PowBlock): Either[ValidationException, BC]
-  def validate(block: PowBlock): Either[ValidationException, B]
+  def append(block: B): Either[ValidationException, BC]
+  def validate(block: B): Either[ValidationException, B]
 }
